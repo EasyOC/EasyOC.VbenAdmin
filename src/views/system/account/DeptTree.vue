fb373c6ff5f5de634d2e8b4d7d5ad07bff2675bb
yan.gao <yan.gao@jizhousoft.com>
2021-12-28 15:25:04 +0800

    add ContentType Pages

--- src/views/system/account/DeptTree.vue
+++ src/views/system/account/DeptTree.vue
@@ -15,7 +15,7 @@
   import { defineComponent, onMounted, ref } from 'vue';
 
   import { BasicTree, TreeItem } from '/@/components/Tree';
-  import { getDeptList } from '../../../api/demo/system';
+  import { getDeptList } from '../../../api/system';
 
   export default defineComponent({
     name: 'DeptTree',
