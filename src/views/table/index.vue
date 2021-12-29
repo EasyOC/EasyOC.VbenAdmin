fcfb2757989a281ff0e057bc0590e8e2ee8c38cc
zyt <zhangyatao@jizhousoft.com>
2021-12-29 12:59:59 +0800

    增加vue-class-component包

---
+++ src/views/table/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <label>
+    {{ aaa }}
+  </label>
+</template>
+<script lang="ts">
+  import { Vue } from 'vue-class-component';
+
+  
+  export default class tables extends Vue {
+    private aaa = '21';
+
+
+    setup() {
+      this.aaa = '22';
+
+      return this.aaa;
+    }
+  }
+</script>
