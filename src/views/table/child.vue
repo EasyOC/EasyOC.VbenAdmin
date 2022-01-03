6e175abdb711b094fcec0665fc3707718d6f4b49
zyt <zhangyatao@jizhousoft.com>
2022-01-03 21:14:26 +0800

    增加新旧版本父子组件传值demo

---
+++ src/views/table/child.vue
@@ -0,0 +1,47 @@
+<template>
+  <label>
+    {{ aaa }}
+  </label>
+  <a-button type="primary" @click="btnClick">1111111111111111111</a-button>
+</template>
+<script lang="ts">
+  import { Vue, Prop,Options  } from 'vue-property-decorator';
+
+  @Options({
+    name: 'Child',
+    components:{},
+    setUp(_, props, ctx) {
+      console.log('props: ', props);
+      console.log('ctx: ', ctx);
+      // setTimeout(()=>{
+      //   console.log('value: ', this.aaa);
+      // },4000)
+      console.log('value: ', 1111);
+    }
+  })
+  export default class Child extends Vue {
+    @Prop({ default: ""}) public aaa:string;
+
+
+    // setUp(){
+    //   // setTimeout(()=>{
+    //   //   console.log('value: ', this.aaa);
+    //   // },4000)
+    //   console.log('value: ', 1111);
+    // }
+
+    public get aaaModel(): string{
+      return this.aaa;
+    }
+
+    public set aaaModel(value: string) {
+      console.log('value: ', value);
+      this.$emit('update:aaa', value);
+    }
+
+
+    btnClick() {
+      this.aaaModel = '1111111111111111111';
+    }
+  }
+</script>
--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -1,20 +1,45 @@
 <template>
+  <div>
   <label>
     {{ aaa }}
   </label>
+  <br>
+  3333
+  <br>
+  <a-button type="primary" @click="btnClick">22222222222222222</a-button>
+
+  <Suspense>
+			<template v-slot:default>
+				<child v-model:aaa="aaa"></child> 
+			</template>
+			<template v-slot:fallback>
+				<h3>加载中.....</h3>
+			</template>
+	</Suspense>
+  <!-- <child v-model:aaa="aaa"></child> -->
+  </div>
 </template>
 <script lang="ts">
-  import { Vue } from 'vue-class-component';
+  import { Vue,Options } from 'vue-property-decorator';
+  import { defineAsyncComponent } from 'vue'
+  // import child from '/@/views/table/child.vue';
 
-  
+  const child = defineAsyncComponent(()=>import('/@/views/table/child1.vue'));
+
+  @Options({
+    name: 'tables',
+    components: {
+       child
+    }
+  })
   export default class tables extends Vue {
     private aaa = '21';
+    
 
-
-    setup() {
-      this.aaa = '22';
-
-      return this.aaa;
+    btnClick() {
+      this.aaa = '22222222222222222';
     }
+
+    
   }
 </script>
