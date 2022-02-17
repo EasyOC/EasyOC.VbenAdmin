95847665053e1e97746d2256429f5acdaf6f2ad5
Tony Han <huarui2219@qq.com>
2022-02-17 21:25:42 +0800

    unblock grapqlgetQuery

--- admin/src/views/demo/form/index.vue
+++ admin/src/views/demo/form/index.vue
@@ -1,48 +1,73 @@
 <template>
   <Amis :amisjson="amisjson" @amisMounted="amisMounted" />
-  <MonacoEditor
-    @editorDidMount="editorDidMount"
-    @change="editorChange"
-    v-model:value="editorJson"
-  />
+  <!-- <CodeEditor @change="editorChange" v-model:value="editorJson" /> -->
 </template>
 <script setup lang="ts">
-import { computed, defineComponent, unref, ref } from 'vue'
-import MonacoEditor from '@/components/MonacoEditor/index.vue'
+import { computed, ref } from 'vue'
+// import MonacoEditor from '@/components/MonacoEditor/index.vue'
+import { CodeEditor } from '@/components/CodeEditor'
 import { Amis } from '@/components/Amis'
 import { parser } from 'xijs'
 
 const editorJson = ref<any>(`
     {
-  "type": "page",
-  "body": {
-    "type": "form",
-    "mode": "horizontal",
-    "api": "https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/form/saveForm",
-    "debug": true,
-    "body": [
-      {
-        "type": "input-date",
-        "name": "date",
-        "label": "日期"
-      }
-    ]
-  }
+  "type": "form", 
+  "api": {
+    "method": "post",
+    "url": "/connect/token",
+    "dataType": "form-data",
+    "replaceData": false,
+    "data": {
+      "grant_type": "password",
+      "client_id": "vue_client_app",
+      "scopes": "openid profile roles api permissions"
+    }
+  },
+  "title": "常规模式",
+  "mode": "normal",
+  "body": [
+    {
+      "type": "input-text",
+      "name": "username",
+      "required": true,
+      "placeholder": "请输入邮箱",
+      "label": "邮箱",
+      "size": "full"
+    },
+    {
+      "type": "input-password",
+      "name": "password",
+      "label": "密码",
+      "required": true,
+      "placeholder": "请输入密码",
+      "size": "full"
+    },
+    {
+      "type": "checkbox",
+      "name": "rememberMe",
+      "label": "记住登录"
+    },
+    {
+      "type": "submit",
+      "label": "登录"
+    }
+  ]
 }`)
-const editor = ref<any>(null)
+// const editor = ref<any>(null)
 const amisjson = computed(() => {
   return parser.parse(editorJson.value)
 })
-function editorDidMount(loadedEditor) {
-  editor.value = loadedEditor
-  // try {
-  //   editor.value.setValue(editorJson.value)
-  // } catch (error) {}
+// function editorDidMount(loadedEditor) {
+//   editor.value = loadedEditor
+//   // try {
+//   //   editor.value.setValue(editorJson.value)
+//   // } catch (error) {}
+// }
+function editorChange() {
+  // editorJson.value = value
+  amisScoped.value.updateProps(amisjson.value)
 }
-function editorChange(value) {
-  editorJson.value = value
-}
-const amisScoped = ref<any>({})
+const amisScoped = ref<any>(null)
 function amisMounted(amisScope) {
   amisScoped.value = amisScope
 }
