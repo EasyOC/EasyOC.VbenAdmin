f77fa40bc64b297a509e7b0becb8df48cacc4d47
Tony Han <huarui2219@qq.com>
2022-02-21 14:44:35 +0800

    add amis Editor

--- admin/src/components/Amis/src/Editor.vue
+++ admin/src/components/Amis/src/Editor.vue
@@ -1,9 +1,10 @@
+<script lang="tsx">
 import { Editor } from 'amis-editor'
 import ReactDOM from 'react-dom'
 import { ref, onMounted } from 'vue'
-
 const props = ref<any>({})
 
 onMounted(() => {
   ReactDOM.render(() => <Editor {...props.value} />)
 })
+</script>
