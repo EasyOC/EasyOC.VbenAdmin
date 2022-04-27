88b923c83bf99229c4d44f61d639bbd6bbf94ef6
zyt <zhangyatao@jizhousoft.com>
2022-04-27 15:17:47 +0800

    修改授权登录和登出

---
+++ apps/ant-admin/src/views/sys/login/logoutcallback.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+  </div>
+</template>
+<script  lang='ts' setup>
+import authService from '@/api/authService'
+import { useGo } from '@/hooks/web/usePage'
+import { useUserStore } from '@/store/user'
+import { onBeforeMount } from 'vue'
+    
+onBeforeMount(async()=>{
+    await authService.completeLogout();
+    const userStore = useUserStore();
+    await userStore.logout(true,true);
+})
+</script>
