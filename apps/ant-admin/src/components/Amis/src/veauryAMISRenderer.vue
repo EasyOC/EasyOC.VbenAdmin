892fd0f8ea357963b682008cd7e640f546194696
Tony Han <huarui2219@qq.com>
2022-05-17 15:37:58 +0800

    add veaury

---
+++ apps/ant-admin/src/components/Amis/src/veauryAMISRenderer.vue
@@ -0,0 +1,25 @@
+<template>
+  <Basic :foo="foo">
+    <div>
+      children内容
+    </div>
+  </Basic>
+</template>
+<script>
+import {applyReactInVue} from 'veaury'
+// 这是一个React组件
+import BasicReactComponent from './react_app/Basic.jsx'
+import {ref} from 'vue'
+
+export default {
+  components: {
+    // 使用高阶组件 'applyReactInVue'
+    Basic: applyReactInVue(BasicReactComponent)
+  },
+  setup() {
+    return {
+      foo: ref('Hello!')
+    }
+  }
+}
+</script>
